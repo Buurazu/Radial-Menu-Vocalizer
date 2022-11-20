cd5859f7c699125519baf91b4e400b2cd913db88
Buurazu <excel_saga_rules@hotmail.com>
2022-11-19 23:16:12 -0500

    v2.4.1

--- RadialMenus/Move.lua
+++ RadialMenus/Move.lua
@@ -35,10 +35,16 @@
             callback = callback(VoiceCommandsMod,VoiceCommandsMod,"say_line","v51")
 		},
 		{
+			text = "Guys, help!",
+			stay_open = false, show_text = true,
+            callback = callback(VoiceCommandsMod,VoiceCommandsMod,"say_line","f11e_plu")
+		},
+		{
 			text = "Get up!",
 			stay_open = false, show_text = true,
             callback = callback(VoiceCommandsMod,VoiceCommandsMod,"say_line","f41_any")
-		}
+		},
+		
 	}
 	
 	VoiceCommandsMod:Refresh(VoiceCommandsMod.my_items,"Radial Menu: MOVE!")
